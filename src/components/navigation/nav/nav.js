4d784eedbf346ee2f02df04b40c47b1ccf90d44f
Connor <cfech44@gmail.com>
2021-04-11 18:11:06 -0400

    adding animations to buttons and changing image to java

--- src/components/navigation/nav/nav.js
+++ src/components/navigation/nav/nav.js
@@ -2,11 +2,7 @@ import React from 'react'
 import "./nav.css"
 import { NavLink, withRouter } from "react-router-dom"
 
-
-
 const nav = (props) => {
-
-
     let backGroundImageSelector = "topPortion"
 
     if (props.match.path === "/technologies") {
@@ -18,21 +14,15 @@ const nav = (props) => {
         <NavLink exact className="m-lg-2 p-lg-2 navLink" to="/" >Home</NavLink>
         <NavLink exact className="m-lg-2 p-lg-2 navLink" to="/technologies" >Technologies</NavLink>
         <NavLink exact className="m-lg-2 p-lg-2 navLink" to="/certifications" >Certifications</NavLink>
-        <a className="m-lg-2 p-lg-2 navLink" href="https://www.linkedin.com/in/connor-fech-99936514a/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a className="m-lg-2 p-lg-2 navLink test" href="https://www.linkedin.com/in/connor-fech-99936514a/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
         <a className="m-lg-2 p-lg-2 navLink" href="https://docs.google.com/document/d/18BXcytPTfkACci4s0MPdzPagMqCKndZFH2qNt9IM_hA/edit?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a>
         <a className="m-lg-2 p-lg-2 navLink " href="https://github.com/cfech" target="_blank" rel="noopener noreferrer">Github</a>
-
-
-
     </div>
 
     let menu =
         <div className="hamburger" onClick={props.openSideDrawer}>
             <i className="fab fa-buromobelexperte"></i>
         </div>
-
-
-
 
     return (
         <div className={backGroundImageSelector}>
@@ -41,8 +31,6 @@ const nav = (props) => {
             </nav>
             {menu}
             {links}
-
-
         </div>
     )
 }
--- src/components/technology/technologies.js
+++ src/components/technology/technologies.js
@@ -17,19 +17,22 @@ function TechnologyDiv() {
                 <img src={require("../../TechnologyPhotos/javascript.png")} alt="javascript" className="techImages" />
             </div>
             <div className="col-lg-2 col-md-4 col-6">
+                <img src={require("../../TechnologyPhotos/java.png")} alt="heroku" className="techImages" />
+            </div>
+
+            <div className="col-lg-2 col-md-4 col-6">
+
+                <img src={require("../../TechnologyPhotos/mongo.png")} alt="mongoDB" className="techImages" />
+            </div>
+            <div className="col-lg-2 col-md-4 col-6">
                 <img src={require("../../TechnologyPhotos/git.jpg")} alt="git" className="techImages" />
             </div>
             <div className="col-lg-2 col-md-4 col-6">
 
                 <img src={require("../../TechnologyPhotos/nodejs.png")} alt="node.js" className="techImages" />
             </div>
-            <div className="col-lg-2 col-md-4 col-6">
-
-                <img src={require("../../TechnologyPhotos/mongo.png")} alt="mongoDB" className="techImages" />
-            </div>
-            <div className="col-lg-2 col-md-4 col-6">
-                <img src={require("../../TechnologyPhotos/heroku.png")} alt="heroku" className="techImages" />
-            </div>
+            
+            
         </div>
     )
 
